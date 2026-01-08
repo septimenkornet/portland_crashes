7f758aae7e13525629e20579362d4f4c913871cc
septimenkornet <mjs@smithbowen.net>
2026-01-08 16:55:52 -0500

    Markers with missing popups seem to be those initially outside any cluster.

--- crash_script.js
+++ crash_script.js
@@ -132,6 +132,7 @@ legend.addTo(map);
 
 // Create a Marker Cluster Group
 var markers = L.markerClusterGroup({
+    spiderfyDistanceMultiplier: 2,
 	maxZoom: 24,
     disableClusteringAtZoom: 18, // Markers will decluster at zoom level 18 and below
     // maxClusterRadius: 30 // Use default
