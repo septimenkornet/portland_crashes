f13eee286304135b841033a9c66ed69b75d6aeaf
septimenkornet <mjs@smithbowen.net>
2026-01-09 15:16:36 -0500

    Better time/date on popup

--- crash_script.js
+++ crash_script.js
@@ -50,10 +50,9 @@ var getboundary = function (city, map) { // Add municipal boundary
 }
 
 var getlabel = function (feature) { // For use constructing popup
-    var retstr = "DOT ID: " + feature.properties["MDOT ID"] +
-        ", date: " +
-        feature.properties["Crash Date"].split(' ', 2)[0] +
-        `, time: ${feature.properties["Time of Crash"]}`;
+    var retstr = `DOT ID: ${feature.properties["MDOT ID"]}: ` +
+        `${feature.properties["Crash Date"].split(' ', 2)[0]}, ` +
+        `${feature.properties["Time of Crash"]}`;
     if ( feature.properties["Bicycle Yes/No"] == "Y" ) {
         retstr += "<br>Cyclist: "
     }
